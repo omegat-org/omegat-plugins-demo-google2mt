906c08fbfa8f0f250681a6c0cbb43ec5b3dcf399
Hiroshi Miura <miurahr@linux.com>
2021-06-15 00:34:36 +0900

    Update build.gradle.kts
    Drop unused dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     java
-    checkstyle
     distribution
     maven
     id("org.omegat.gradle") version "1.5.3"
@@ -14,16 +13,6 @@ omegat {
 }
 
 dependencies {
-    packIntoJar("org.slf4j:slf4j-api:1.7.21")
-    implementation("commons-io:commons-io:2.5")
-    implementation("commons-lang:commons-lang:2.6")
-    implementation("org.slf4j:slf4j-nop:1.7.21")
-    testImplementation("junit:junit:4.12")
-}
-
-checkstyle {
-    isIgnoreFailures = true
-    toolVersion = "7.1"
 }
 
 distributions {
